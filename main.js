aa09b8b474d1f7e85006873a4a181edd59ab3bf8
Temilollu <temiloluwaodumosu25@gmail.com>
2020-06-18 12:25:02 +0100

    worked on the duration and added Naira symbol

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var interestText = document.getElementById("interest");
 var balanceText = document.getElementById("balance");
 var interested = 0;
 var balanced = 0;
+var welcome = document.querySelector('.result-detail')
 
 btn.addEventListener("click", calculateInterest);
 close.addEventListener("click", function() {
@@ -22,18 +23,22 @@ function calculateInterest() {
 	
 	
 	var rate = 1;
-	
+	let days;
 	if(duration == "1") {
 		rate = 6;
+		days = '10 - 30 days'
 	}
 	else if(duration == "2") {
 		rate = 8;
+		days = '31 - 60 days'
 	}
 	else if(duration == "3") {
 		rate = 10;
+		days = '61 - 90 days'
 	}
 	else if(duration == "4") {
 		rate = 13;
+		days = '91days - 2 years'
 	}
 
 	if(isNaN(amount) || amount == "0" || amount == "") {
@@ -46,9 +51,11 @@ function calculateInterest() {
 
 	else {
 		interested = (amount*rate*duration)/100;
-		balanced = (interested+amount);
+		balanced = 'N' +(interested+amount);
+		interested = 'N' +(amount*rate*duration)/100;
 
 		result.classList.add("visible");
+		welcome.textContent = `At the end ${days}, your interest will be`
 		interestText.textContent = interested;
 		balanceText.textContent = balanced;
 		safelock.textContent = "SAFELOCK " + amount + " NOW!!";
